1193dae92125318e2be80a004b83dbf77156403e
ETsETs777 <cymbalzena087@gmail.com>
2025-05-27 11:42:12 +0300

    Update interface to YouTube style

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,24 +6,20 @@ import { GenerateHistory } from './components/GenerateHistory';
 import { ScanHistory } from './components/ScanHistory';
 import { Header } from './components/on/Apostrophes';
 import { Footer } from './components/on/Apostrophes';
+
 const Layout = () => {
     return (
         <div className="app">
             <Header />
-            <div style={{ 
-                background: "url('back.webp') center/cover no-repeat fixed",
-                minHeight: 'calc(100vh - 120px)', 
-                padding: '20px 0'
-            }}>
-                <Navigation />
+            <Navigation />
+            <main className="main-content">
                 <Routes>
                     <Route path="/qr-code/generate" element={<QrCodeGenerator />} />
                     <Route path="/qr-code/scan" element={<QrCodeScanner />} />
                     <Route path="/qr-code/scanHistory" element={<ScanHistory />} />
                     <Route path="/qr-code/generateHistory" element={<GenerateHistory />} />
                 </Routes>
-            </div>
-            
+            </main>
             <Footer />
         </div>
     );
